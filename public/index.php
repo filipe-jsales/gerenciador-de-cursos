d73e9fba9f45bfdfa27f923c014202c4a83b5878
Filipe <eduardojs999@gmail.com>
2022-10-30 14:16:18 -0300

    adjust code

--- public/index.php
+++ public/index.php
@@ -2,11 +2,6 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-// use Alura\Cursos\Controller\FormularioInsercao;
-// use Alura\Cursos\Controller\InterfaceControladorRequisicao;
-// use Alura\Cursos\Controller\ListarCursos;
-// use Alura\Cursos\Controller\Persistencia;
-
 $caminho = $_SERVER['PATH_INFO'];
 $rotas = require __DIR__ . '/../config/routes.php';
 
